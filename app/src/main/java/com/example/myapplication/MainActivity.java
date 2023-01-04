47e6eb1cb16bd8316aea467e1b04f603bcc777bd
FelipeIgansi <lipeigansi@gmail.com>
2023-01-04 00:27:46 -0300

    refatoracao

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.model.Operacoes;
 import com.example.myapplication.util.Convert;
 
@@ -44,66 +43,53 @@ public class MainActivity extends AppCompatActivity {
 
     private void setarOperacao(int idBotaoOperacao, String operacaoRealizada) {
         botao = findViewById(idBotaoOperacao);
-        botao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (valorImpresso != "")
-                    operacoes.setValor(convert.toInt(valorImpresso));
-                if (operacaoRealizada == "C"){
+        botao.setOnClickListener(view -> {
+            if (!valorImpresso.equals(""))
+                operacoes.setValor(convert.toInt(valorImpresso));
+            switch (operacaoRealizada) {
+                case "C":
                     operacoes.limpaLista();
                     valorImpresso = "";
                     exibeValorEmOperacoes(valorImpresso);
                     exibeValorEmResultados(valorImpresso);
-                }
-                else if (operacaoRealizada == "+"){
+                    break;
+                case "+":
+                case "-":
+                case "x":
+                case "/":
                     operacoes.setOperacao(operacaoRealizada);
                     valorImpresso = "";
                     exibeValorEmOperacoes(valorImpresso);
-
-                }
-                else if (operacaoRealizada == "-") {
-                    operacoes.setOperacao(operacaoRealizada);
-                    valorImpresso = "";
-                    exibeValorEmOperacoes(valorImpresso);
-                }
-                else if (operacaoRealizada == "x") {
-                    operacoes.setOperacao(operacaoRealizada);
-                    valorImpresso = "";
-                    exibeValorEmOperacoes(valorImpresso);
-                }
-                else if (operacaoRealizada == "/") {
-                    operacoes.setOperacao(operacaoRealizada);
-                    valorImpresso = "";
-                    exibeValorEmOperacoes(valorImpresso);
-                }
-                else  if(operacaoRealizada == "="){
-                    if (operacoes.getOperacao() == "+")
-                        total = operacoes.soma();
-                    else if (operacoes.getOperacao() == "-")
-                        total = operacoes.subtracao();
-                    else if (operacoes.getOperacao() == "x")
-                        total = operacoes.multiplicacao();
-                    else if (operacoes.getOperacao() == "/")
-                        total = operacoes.divisao();
+                    break;
+                case "=":
+                    switch (operacoes.getOperacao()) {
+                        case "+":
+                            total = operacoes.soma();
+                            break;
+                        case "-":
+                            total = operacoes.subtracao();
+                            break;
+                        case "x":
+                            total = operacoes.multiplicacao();
+                            break;
+                        case "/":
+                            total = operacoes.divisao();
+                            break;
+                    }
 
                     exibeValorEmResultados(convert.toStr(total));
                     operacoes.limpaLista();
                     valorImpresso = "";
                     exibeValorEmOperacoes(valorImpresso);
-                }
-
+                    break;
             }
+
         });
     }
 
     private void setarValor(int idBotao, int valor) {
         botao = findViewById(idBotao);
-        botao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                exibeValorEmOperacoes(convert.toStr(valor));
-            }
-        });
+        botao.setOnClickListener(view -> exibeValorEmOperacoes(convert.toStr(valor)));
     }
 
     private void exibeValorEmOperacoes(String valor){
